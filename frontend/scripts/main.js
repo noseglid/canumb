d067321f975e9f6d0aab5b9d56a5b5c3c1dac27a
Alexander Olsson <noseglid@gmail.com>
2014-02-01 12:57:51 +0100

    Frontend: Display githubs readme

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -25,28 +25,31 @@ require([
   "analytics",
   "text!templates/result.tpl"], function($, _, growl, analytics, resultTpl) {
 
-  var resultFadeTime = 100;
-  var queryDelay     = 300;
+  var fadeTime   = 100;
+  var queryDelay = 300;
 
   var currentResult;
 
   function setResult()
   {
     var align = $('#align-button input').is(':checked');
-    $('#result').html(_.template(resultTpl, {
-      bin : currentResult.bin[align ? 'group8' : 'standard'],
-      oct : currentResult.oct['standard'],
-      dec : currentResult.dec['standard'],
-      hex : currentResult.hex['standard'],
-      checked : align ? 'checked="checked"' : ''
-    })).fadeIn(resultFadeTime);
+    $('#readme').fadeOut(0, function() {
+      $('#result').html(_.template(resultTpl, {
+        bin : currentResult.bin[align ? 'group8' : 'standard'],
+        oct : currentResult.oct['standard'],
+        dec : currentResult.dec['standard'],
+        hex : currentResult.hex['standard'],
+        checked : align ? 'checked="checked"' : ''
+      })).fadeIn(fadeTime);
+    });
   }
 
   function unsetResult()
   {
     $('#loader').css('visibility', 'hidden');
-    $('#result').fadeOut(resultFadeTime, function() {
+    $('#result').fadeOut(fadeTime, function() {
       $('#result').text('');
+      $('#readme').fadeIn(fadeTime);
     });
   }
 
@@ -100,6 +103,18 @@ require([
       var checkbox = $('#align-button input');
       checkbox.prop('checked', !checkbox.prop('checked'));
       setResult();
+    });
+
+    $.ajax('https://api.github.com/repos/noseglid/canumb/readme', {
+      'headers' : {
+        'Accept' : 'application/vnd.github.v3.html+json'
+      },
+      'success' : function(data, textStatus, jqXHR) {
+        $('#readme').html(data).fadeIn(fadeTime);
+      },
+      'error' : function(data, textStatus, jqXHR) {
+        $('#readme').text('Could not fetch readme at this time...');
+      }
     });
   });
 });

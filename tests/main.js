060102de07b2e8c378531d73bd8fcccaa7bf90b9
Alexander Olsson <noseglid@gmail.com>
2014-02-22 16:32:57 +0100

    Tests: Randomize server port

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 
 /* So, we need a server for the API tests */
+process.env.PORT = 1024 + Math.ceil(Math.random() * 64511);
 var server = require('../src/main.js');
+server.on('listening', startTests);
 
-/* Run these tests ... */
-var tests = [
-  'tests/lib/numbers/',
-  'tests/api/'
-];
+function startTests()
+{
+  /* Run these tests ... */
+  var tests = [
+    'tests/lib/numbers/',
+    'tests/api/'
+  ];
 
-/* ... unless overridden by environment */
-if (process.argv.length > 2) {
-  tests = process.argv.slice(2);
+  /* ... unless overridden by argumentlist */
+  if (process.argv.length > 2) {
+    tests = process.argv.slice(2);
+  }
+
+  var reporter = require('nodeunit').reporters.default;
+  reporter.run(tests, false, function() {
+    server.close();
+  });
 }
-
-var reporter = require('nodeunit').reporters.default;
-reporter.run(tests, false, function() {
-  server.close();
-});
